3692a2010a94e49e4a8e2a93e2644699d677e0ca
antemin <69094085+antemin@users.noreply.github.com>
2020-08-01 16:24:34 -0700

    Update main.tf
    Add null_resource

--- main.tf
+++ main.tf
@@ -1,34 +1,37 @@
-provider "aws" {
-  version = "2.33.0"
+# provider "aws" {
+#   version = "2.33.0"
 
-  region = var.aws_region
-}
+#   region = var.aws_region
+# }
 
-provider "random" {
-  version = "2.2"
-}
+# provider "random" {
+#   version = "2.2"
+# }
 
-resource "random_pet" "table_name" {}
+# resource "random_pet" "table_name" {}
 
-resource "aws_dynamodb_table" "tfc_example_table" {
-  name = "${var.db_table_name}-${random_pet.table_name.id}"
+# resource "aws_dynamodb_table" "tfc_example_table" {
+#   name = "${var.db_table_name}-${random_pet.table_name.id}"
 
-  read_capacity  = var.db_read_capacity
-  write_capacity = var.db_write_capacity
-  hash_key       = "UUID"
-  range_key      = "UserName"
+#   read_capacity  = var.db_read_capacity
+#   write_capacity = var.db_write_capacity
+#   hash_key       = "UUID"
+#   range_key      = "UserName"
 
-  attribute {
-    name = "UUID"
-    type = "S"
-  }
+#   attribute {
+#     name = "UUID"
+#     type = "S"
+#   }
 
-  attribute {
-    name = "UserName"
-    type = "S"
-  }
+#   attribute {
+#     name = "UserName"
+#     type = "S"
+#   }
 
-  tags = {
-    user_name = var.tag_user_name
-  }
+#   tags = {
+#     user_name = var.tag_user_name
+#   }
+# }
+
+resource "null_resource" "a" {
 }
